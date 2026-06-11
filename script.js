1e9eddafcb41e7870aa8c399a4501e0d268ecf76
Avtar111/SIKKA <258620616+avtarsingh1001-rgb@users.noreply.github.com>
2026-06-11 16:42:07 +0530

    Add function to alert on task completion

---
+++ script.js
@@ -0,0 +1,5 @@
+// Task poora hone par alert dikhane ka function
+function completeTask(taskName) {
+    alert("Badhai ho! Aapka '" + taskName + "' task shuru ho gaya hai. Poora hote hi Sikka aapke wallet me add ho jayenge.");
+}
+
