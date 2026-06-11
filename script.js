f1a028727de21fe9a54d74035d079bca217f36ea
Avtar111/SIKKA <258620616+avtarsingh1001-rgb@users.noreply.github.com>
2026-06-11 18:05:55 +0530

    Add functions for task completion and login modal

--- script.js
+++ script.js
@@ -1,5 +1,24 @@
 // Task poora hone par alert dikhane ka function
 function completeTask(taskName) {
     alert("Badhai ho! Aapka '" + taskName + "' task shuru ho gaya hai. Poora hote hi Sikka aapke wallet me add ho jayenge.");
+}// Login Modal ko dikhane ke liye
+function openLoginModal() {
+    document.getElementById('loginModal').style.display = 'flex';
 }
+
+// Login Modal ko band karne ke liye
+function closeLoginModal() {
+    document.getElementById('loginModal').style.display = 'none';
+}
+
+// User jab login submit karega tab ke liye action
+function handleLogin(event) {
+    event.preventDefault(); // page refresh hone se rokega
+    alert('Aapka Login Safal Hua!');
+    closeLoginModal();
+    
+    // Login hone ke baad button ka text 'Profile' kar dega
+    document.getElementById('loginBtn').innerText = 'Profile';
+}
+
 
